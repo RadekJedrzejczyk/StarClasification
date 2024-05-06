fd71106d7f65177ff4c0f522bb21ad7ac5fbdf5a
@RadekJedrz <radoslaw.jedrzejczyk.rj@gmail.com>
2024-05-06 09:24:35 +0200

    Added function finding best number of neighbours for KNN algorithm.

---
+++ bestNeighborsNumber.py
@@ -0,0 +1,13 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+def findBestNeighboursNumber(x_train,y_train,x_test,y_test,max_number_of_neighbours):
+    solution_list =[]
+    for n in range(1, max_number_of_neighbours):
+        knn_lib=KNeighborsClassifier(n)
+        knn_lib.fit(x_train,y_train.values.ravel())
+        solution_list.append(knn_lib.score(x_test,y_test))
+        print(f"{n}/{max_number_of_neighbours}")
+    
+    max_value = max(solution_list)
+    max_index = solution_list.index(max_value)
+    return max_index, solution_list
